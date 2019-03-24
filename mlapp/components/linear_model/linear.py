5609da189964bc9f09ba55e60039b42aac5fa584
kenshoo-build <_devSA@kenshoo.com>
2019-03-24 15:59:11 +0200

    first ...

---
+++ mlapp/components/linear_model/linear.py
@@ -0,0 +1,21 @@
+from parallelm.components.connectable_component import ConnectableComponent
+from parallelm.mlops import mlops
+from parallelm.mlops.stats.multi_line_graph import MultiLineGraph
+
+
+class Linear(ConnectableComponent):
+    def __init__(self, engine):
+        super(self.__class__, self).__init__(engine)
+
+    def _materialize(self, parent_data_objs, user_data):
+        for param in parent_data_objs:
+            prent_param = "parent param is: {param}".format(param=param)
+            print(prent_param)
+            self._logger.info(prent_param)
+
+        mlt = MultiLineGraph().name("Multi-line graph example").labels(["lable-1", "lable-2", "lable-3"])
+        for x in range(100):
+            mlt.data([x, x + 1, 0.5 * x])
+            mlops.set_stat(mlt)
+
+        return ["s3://Kenshoo/this is the linear model path/model.pmml"]
