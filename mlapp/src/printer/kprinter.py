7c98346229206dc7da8656347a7ab8b8c1a15694
kenshoo-build <_devSA@kenshoo.com>
2019-04-08 14:45:09 +0300

    change print

--- mlapp/src/printer/kprinter.py
+++ mlapp/src/printer/kprinter.py
@@ -1,13 +1,9 @@
-
-
 print(__file__)
 
 def print_data(logger, message):
     logger.info(__file__)
-    logger.info(locals())
-    logger.info(globals())
     from printer.walla.dalla import print_data_walla
-    print_data_walla(logger, message)
+    print_data_walla(logger=logger, message=message)
 
 def get_name():
     return __file__
