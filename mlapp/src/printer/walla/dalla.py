f2e4208e50ed0268aa769657dfbee368d8653c09
kenshoo-build <_devSA@kenshoo.com>
2019-04-08 14:30:56 +0300

    k3

--- mlapp/src/printer/walla/dalla.py
+++ mlapp/src/printer/walla/dalla.py
@@ -4,3 +4,4 @@
 def print_data_walla(logger, message):
     logger.info(__file__)
     logger.info(message)
+    logger.info("*******************************************ADIEL*************************************")
